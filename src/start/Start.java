30e71cdc5ce96638ec1f796e7cc19abdc9563117
Cab <cabalor@gmail.com>
2018-02-18 18:19:52 +0100

    asda asda

--- src/start/Start.java
+++ src/start/Start.java
@@ -1,10 +1,19 @@
 package start;
 
+
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Weapon miecz = new Swords("miecz", 1,3);
+		
+		Creatures bandyta = new Humans("Bandyta", 10, miecz);
+		
+		Player player = new Player("Name", 10, miecz);
+		System.out.println(player);
+		
+		Fight.hit(player, bandyta);
+		
+		
 	}
 
 }
